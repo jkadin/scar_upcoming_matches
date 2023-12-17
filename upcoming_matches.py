33e168b91c61d44d132cd2402023797a73cd7bb7
jkadin <jkadin>
2023-12-17 14:04:43 -0800

    Inital commit

---
+++ upcoming_matches.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from itertools import chain, zip_longest
+from datetime import datetime, timedelta
+
+from dotenv import load_dotenv
+import challonge
+
+# Dotenv lets you create a file named ".env" in the same directory as this file, and populate it with data that gets added you your env vars.
+# The challonge username and api keys are pulled from here or from env vars, so for local debugging you can use one set of data (and add it to your .gitignore)
+# but on the live server you can either use your own .env file or just set env vars in the control panel or launcher
+load_dotenv()
+
+NEXT_MATCH_START = timedelta(minutes=1)
+MATCH_DELAY = timedelta(minutes=3)
+
+
+def interleave_matches(tournaments):
+    matches_list = [t["matches"] for t in tournaments.values()]
+    interleaved_with_fill = zip_longest(*matches_list)
+    list_of_tuples = chain.from_iterable(interleaved_with_fill)
+    remove_fill = [x for x in list_of_tuples if x is not None]
+    return remove_fill
+
+
+def main():
+    challonge.set_credentials(os.getenv("CHALLONGE_USERNAME"), os.getenv("CHALLONGE_API_KEY"))
+    
+    tournaments = challonge.tournaments.index(state="in_progress")
+    if not tournaments:
+        print("No in-progress tournaments found.")
+        sys.exit()
+    tournaments = {t["id"]: t for t in tournaments}
+
+    for t in tournaments:
+        # Populate matches
+        matches = challonge.matches.index(t, state="open")
+        # Populate participants
+        participants = challonge.participants.index(t)
+        participants = {p["id"]: p for p in participants}
+        for y, match in enumerate(matches):
+            matches[y]["player1_name"] = participants.get(match["player1_id"], {}).get("name", "???")
+            matches[y]["player2_name"] = participants.get(match["player2_id"], {}).get("name", "???")
+        tournaments[t]["matches"] = matches
+
+    # Create combined match list
+    ordered_matches = interleave_matches(tournaments)
+    match_start = datetime.now() + NEXT_MATCH_START
+    for i, match in enumerate(ordered_matches[:10]):
+        print("%s. %s VS %s - %s" % (i + 1, match["player1_name"], match["player2_name"], match_start.strftime("%I:%M:%S %p")))
+        match_start += MATCH_DELAY
+
+
+if __name__ == "__main__":
+    main()
