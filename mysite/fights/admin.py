4820ffd1fccf843305d165717f3532bb923459f4
Lee Kadin <lkadin@gmail.com>
2024-04-21 17:56:05 -0400

    add model for match details

--- mysite/fights/admin.py
+++ mysite/fights/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Url
+from .models import Url, Match
+
 
 admin.site.register(Url)
+admin.site.register(Match)
--- mysite/fights/models.py
+++ mysite/fights/models.py
@@ -8,3 +8,10 @@ class Url(models.Model):
 
     def __str__(self) -> str:
         return self.url
+
+
+class Match(models.Model):
+    player1_name = models.CharField(max_length=100)
+    player2_name = models.CharField(max_length=100)
+    match_time = models.DateTimeField()
+    tournemant_name = models.CharField(max_length=100)
