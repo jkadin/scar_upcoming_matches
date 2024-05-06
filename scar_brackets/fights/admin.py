5f4f6b5da92ebb32e6a51334391d658554e7534c
Lee Kadin <lkadin@gmail.com>
2024-05-06 18:34:08 -0400

    Modify participant admin

--- scar_brackets/fights/admin.py
+++ scar_brackets/fights/admin.py
@@ -5,4 +5,11 @@ from .models import Url, Match, Tournament, Participant
 admin.site.register(Url)
 admin.site.register(Match)
 admin.site.register(Tournament)
-admin.site.register(Participant)
+
+
+class Participant_admin(admin.ModelAdmin):
+    list_display = ("participant_id", "participant_name")
+    ordering = ("participant_id", "participant_name")
+
+
+admin.site.register(Participant, Participant_admin)
