500325f064f7f8e027fb9c3838dc373fc2d09f25
Lee Kadin <lkadin@gmail.com>
2024-05-09 12:09:46 -0400

    Added more fields for matches

--- scar_brackets/fights/admin.py
+++ scar_brackets/fights/admin.py
@@ -3,13 +3,20 @@ from .models import Url, Match, Tournament, Participant
 
 
 admin.site.register(Url)
-admin.site.register(Match)
 admin.site.register(Tournament)
 
 
 class Participant_admin(admin.ModelAdmin):
-    list_display = ("participant_id", "participant_name")
+    list_display = ("participant_id", "participant_name", "tournament_id")
     ordering = ("participant_id", "participant_name")
 
 
 admin.site.register(Participant, Participant_admin)
+
+
+class Match_admin(admin.ModelAdmin):
+    list_display = ("match_id", "player1_id", "player2_id", "tournament_id")
+    ordering = ("match_id", "player1_id")
+
+
+admin.site.register(Match, Match_admin)
