277d2bc8e13e266eff77e7397e271ce8ec6a9254
Lee Kadin <lkadin@gmail.com>
2025-01-04 16:05:16 -0500

    change == None to is None

--- scar_brackets/fights/tests/test_match.py
+++ scar_brackets/fights/tests/test_match.py
@@ -8,6 +8,6 @@ def test_match(match):
     assert match.player2_id.participant_name == "Player 2"
     assert match.tournament_id.tournament_name == "Tournament 1"
     assert match.match_state == "open"
-    assert match.updated_at == None
+    assert match.updated_at is None
     assert match.suggested_play_order == 1
     assert match.calculated_play_order == 1
