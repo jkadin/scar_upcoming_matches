2a9f1d732ea5d3d3c3c50c7c1fd72f3493ef26be
Lee Kadin <lkadin@gmail.com>
2025-01-04 16:05:05 -0500

    change url to bot instead of user

--- scar_brackets/fights/tests/test_claim.py
+++ scar_brackets/fights/tests/test_claim.py
@@ -1,6 +1,7 @@
 import pytest
-from django.contrib.auth.models import User
-from django.test import Client
+
+# from django.contrib.auth.models import User
+# from django.test import Client
 from django.urls import reverse
 
 
@@ -9,7 +10,7 @@ def test_claim_user(client, authenticated_user, participant):
     # Set up test data
     # user = User.objects.create_user(username="testuser", password="password")
     # Case 1: Valid claim with "true"
-    url = "/fights/claim_user/Player 2/"
+    url = "/fights/claim_bot/Player 2/"
     response = client.post(url, {"claim": "true"})
 
     assert response.status_code == 200
@@ -27,7 +28,7 @@ def test_claim_user(client, authenticated_user, participant):
     assert response.context["bot"] == participant
 
     # Case 3: Participant does not exist
-    url = reverse("claim_user", kwargs={"participant_name": "Nonexistent Participant"})
+    url = reverse("claim_bot", kwargs={"participant_name": "Nonexistent Participant"})
     response = client.post(url, {"claim": "true"})
 
     assert response.status_code == 200
