6c0edca76ab39f093a97ab0909df1fe373dd35d2
Lee Kadin <lkadin@gmail.com>
2025-01-09 09:22:35 -0500

    Fix test add profile

--- scar_brackets/fights/tests/test_user.py
+++ scar_brackets/fights/tests/test_user.py
@@ -5,9 +5,10 @@ import pytest
 def test_user(
     authenticated_user,
     client,
+    profile,
 ):
     url = "/fights/user/1/"
-    response = client.get(url, authenticated_user)
+    response = client.get(url)
     print(response.content)
     assert response.status_code == 200
     assert 1 == 1
