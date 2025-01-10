280ae5e474ef748b6e04319ba4ec035f5c50df54
Lee Kadin <lkadin@gmail.com>
2025-01-10 08:34:46 -0500

    add additional tests

--- scar_brackets/fights/tests/test_timeout.py
+++ scar_brackets/fights/tests/test_timeout.py
@@ -1,17 +1,16 @@
 # from django.test import TestCase
 import pytest
+from django.utils import timezone
 
-# from fights.models import Match, Tournament, Url, Participant, Profile, User
-
-# from fights.views import time_out
+now = timezone.now()
 
 
-# Create your tests here.
 @pytest.mark.django_db
 def test_time_out(authenticated_user, client, profile):
     print(f"{authenticated_user=}, {profile.last_timeout=}")
     url = "/fights/time_out"
     response = client.get(url)
     print(response.content)
+    profile.refresh_from_db()
     print(f"{authenticated_user=}, {profile.last_timeout=}")
-    assert 1 == 1
+    assert profile.last_timeout.date() == now.date()
