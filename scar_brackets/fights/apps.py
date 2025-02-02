87e7d107db84e8f3b2fb2bd2840da5a9cb6a9402
Lee Kadin <lkadin@gmail.com>
2025-02-02 08:44:16 -0500

    Removed unused code

--- scar_brackets/fights/apps.py
+++ scar_brackets/fights/apps.py
@@ -4,11 +4,3 @@ from django.apps import AppConfig
 class FightConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "fights"
-
-
-class MyAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "fights"
-
-    def ready(self):
-        import fights.signals  # Import the signals module
--- scar_brackets/fights/tests/test_update_from_api.py
+++ scar_brackets/fights/tests/test_update_from_api.py
@@ -10,55 +10,6 @@ def mock_challonge():
         yield mock
 
 
-# # @pytest.fixture
-# # def mock_os():
-# #     with patch("fights.management.commands.update_from_api.os") as mock:
-# #         yield mock
-
-
-# # @pytest.fixture
-# # def mock_load_dotenv():
-# #     with patch("fights.management.commands.update_from_api.load_dotenv") as mock:
-# #         yield mock
-
-
-# # @pytest.fixture
-# # def mock_url_objects():
-# #     with patch("fights.management.commands.update_from_api.Url.objects") as mock:
-# #         yield mock
-
-
-# # @pytest.fixture
-# # def mock_tournament_objects():
-# #     with patch("fights.management.commands.update_from_api.Tournament.objects") as mock:
-# #         yield mock
-
-
-# # @pytest.fixture
-# # def mock_match_objects():
-# #     with patch("fights.management.commands.update_from_api.Match.objects") as mock:
-# #         yield mock
-
-
-# # @pytest.fixture
-# # def mock_bot_objects():
-# #     with patch("fights.management.commands.update_from_api.Bot.objects") as mock:
-# #         yield mock
-
-
-# @pytest.fixture
-# @pytest.mark.django_db
-# def mock_load_bots():
-#     with patch("fights.management.commands.update_from_api.load_bots") as mock:
-#         yield mock
-
-
-# @pytest.fixture
-# def mock_create_null_bot():
-#     with patch("fights.management.commands.update_from_api.create_null_bot") as mock:
-#         yield mock
-
-
 @pytest.mark.django_db
 def test_update_database(
     tournament,
