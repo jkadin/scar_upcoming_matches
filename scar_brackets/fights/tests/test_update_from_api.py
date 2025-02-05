4c0cdd2d816e129d872339385e0fc1baa7207563
Lee Kadin <lkadin@gmail.com>
2025-02-05 15:42:47 -0500

    change values for assert

--- scar_brackets/fights/tests/test_update_from_api.py
+++ scar_brackets/fights/tests/test_update_from_api.py
@@ -1,21 +1,11 @@
 import pytest
-from unittest.mock import patch
 from fights.management.commands.update_from_api import update_database
 from fights.models import Tournament, Match, Bot
 
 
-@pytest.fixture
-def mock_challonge():
-    with patch("fights.management.commands.update_from_api.challonge") as mock:
-        yield mock
-
-
 @pytest.mark.django_db
-def test_update_database(
-    tournament,
-    url,
-):
+def test_update_database(url):
     update_database()
-    assert len(Tournament.objects.all()) == 3
+    assert len(Tournament.objects.all()) == 2
     assert len(Match.objects.all()) == 24
     assert len(Bot.objects.all()) == 15
