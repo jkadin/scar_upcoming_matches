1922a448582d099dfd7de7c82787b7d3a3f2851b
Lee Kadin <lkadin@gmail.com>
2025-03-10 14:10:16 -0400

    New test

---
+++ scar_brackets/fights/tests/test_manual_sort.py
@@ -0,0 +1,20 @@
+import pytest
+import json
+
+
+@pytest.mark.django_db
+def test_manual_order(client, authenticated_user, run_update_database, url):
+    orderedItems = [
+        {"id": "387469591", "order": 0},
+        {"id": "387469583", "order": 1},
+        {"id": "387469598", "order": 2},
+        {"id": "387469582", "order": 3},
+        {"id": "387469595", "order": 4},
+    ]
+    movedItem = {"matchID": "387469591", "newIndex": 1, "oldIndex": 0}
+
+    data = {"orderedItems": orderedItems, "movedItem": movedItem}
+
+    url = "/fights/manual_sort"
+    response = client.post(url, data=json.dumps(data), content_type="application/json")
+    assert response.status_code == 200
