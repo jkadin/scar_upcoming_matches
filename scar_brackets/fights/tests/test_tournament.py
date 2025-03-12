edd3952c92a0f9cd05a6ba7efff9514ae71c2dd0
Lee Kadin <lkadin@gmail.com>
2025-03-12 18:51:49 -0400

    Test wuth challonge data

--- scar_brackets/fights/tests/test_tournament.py
+++ scar_brackets/fights/tests/test_tournament.py
@@ -4,11 +4,11 @@ import pytest
 @pytest.mark.django_db
 def test_tournament(tournament):
 
-    assert str(tournament[0]) == "Tournament 1"
-    assert str(tournament[1]) == "Tournament 2"
-    assert tournament[0].tournament_id == "1"
-    assert tournament[0].tournament_name == "Tournament 1"
-    assert tournament[0].tournament_repair_time == 30
-    assert tournament[1].tournament_id == "2"
-    assert tournament[1].tournament_name == "Tournament 2"
+    assert str(tournament[0]) == "Api Tournament 2"
+    assert str(tournament[1]) == "Api Testing Tournament"
+    assert tournament[0].tournament_id == "13875076"
+    assert tournament[0].tournament_name == "Api Tournament 2"
+    assert tournament[0].tournament_repair_time == 20
+    assert tournament[1].tournament_id == "13874863"
+    assert tournament[1].tournament_name == "Api Testing Tournament"
     assert tournament[1].tournament_repair_time == 20
