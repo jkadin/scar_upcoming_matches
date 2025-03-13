f04b0e684de9bedbeddc1983fcffd2b3d2ae37fc
Lee Kadin <lkadin@gmail.com>
2025-03-13 10:33:19 -0400

    Create profile on login

--- scar_brackets/fights/signals.py
+++ scar_brackets/fights/signals.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.signals import user_logged_in
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 from django.dispatch import receiver
+from .models import Profile
 
 print("Signals is running!")
 
@@ -7,3 +10,14 @@ print("Signals is running!")
 @receiver(user_logged_in)
 def after_login(sender, request, user, **kwargs):
     print(f"User {user.username} has logged in!")
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
