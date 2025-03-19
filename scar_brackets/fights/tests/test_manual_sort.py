9e4bb92bbf91b2b3f2f72677910a7c2d5da82563
Lee Kadin <lkadin@gmail.com>
2025-03-19 09:46:25 -0400

    Further updates on test

--- scar_brackets/fights/tests/test_manual_sort.py
+++ scar_brackets/fights/tests/test_manual_sort.py
@@ -1,9 +1,15 @@
 import pytest
 import json
+from fights.models import Match
 
 
 @pytest.mark.django_db
 def test_manual_order(client, authenticated_user, url, tournament, bots, matches):
+    match_list = Match.objects.filter(match_state="open").order_by(
+        "calculated_play_order"
+    )
+    for match in match_list:
+        print(match, match.calculated_play_order)
     orderedItems = [
         {"id": "387469591", "order": 0},
         {"id": "387469583", "order": 1},
@@ -18,3 +24,9 @@ def test_manual_order(client, authenticated_user, url, tournament, bots, matches
     url = "/fights/manual_sort"
     response = client.post(url, data=json.dumps(data), content_type="application/json")
     assert response.status_code == 200
+
+    match_list = Match.objects.filter(match_state="open").order_by(
+        "calculated_play_order"
+    )
+    for match in match_list:
+        print(match, match.calculated_play_order)
