143614afa7fc6ebd86f3202da89bdb3e6ca734d7
Lee Kadin <lkadin@gmail.com>
2025-04-04 09:05:59 -0400

    Test cancel timeout

--- scar_brackets/fights/tests/test_timeout.py
+++ scar_brackets/fights/tests/test_timeout.py
@@ -1,5 +1,6 @@
 import pytest
 from django.utils import timezone
+from datetime import datetime
 
 now = timezone.now()
 
@@ -8,8 +9,14 @@ now = timezone.now()
 def test_time_out(authenticated_user, client, profile):
     print(f"{authenticated_user=}, {profile.last_timeout=}")
     url = "/fights/time_out"
-    response = client.get(url)
+    response = client.post(url)
     print(response.content)
     profile.refresh_from_db()
     print(f"{authenticated_user=}, {profile.last_timeout=}")
     assert profile.last_timeout.date() == now.date()
+    # Test cancel timeout
+    response = client.post(url, {"cancel": "true"})
+    profile.refresh_from_db()
+    assert profile.last_timeout == timezone.make_aware(
+        datetime.min, timezone.get_default_timezone()
+    )
